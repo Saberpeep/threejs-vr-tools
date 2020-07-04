889c11a280db6237267a86729b629174d4c3aec9
saberpeep <3688260+Saberpeep@users.noreply.github.com>
2020-07-03 22:17:35 -0700

    make constants REALLY read only

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 import { Vector3 } from 'three';
-export default {
+export default readOnlyIfy({
     x: readOnlyIfy(new Vector3(1,0,0)),
     y: readOnlyIfy(new Vector3(0,1,0)),
     z: readOnlyIfy(new Vector3(0,0,1)),
     origin: readOnlyIfy(new Vector3(0,0,0)),
     negY: readOnlyIfy(new Vector3(0,-1,0)),
-}
+});
 
 function readOnlyIfy(obj){
 	return new Proxy(obj, {
